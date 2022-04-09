8268ad6fb9d67f368d8b25047779f1869c2746b4
DejanTeofilovic <dejanteofilovic2@gmail.com>
2022-04-10 01:36:56 +0900

    build

--- client/src/pages/PrivatePage/index.js
+++ client/src/pages/PrivatePage/index.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import { 
+import {
   // Typography, 
-  Box, 
-  Stack 
+  Box,
+  Stack
 } from '@mui/material';
 
 export default function PriavatePage() {
   return (
-    <Box mt={20}>
-      {/* <Typography color="white" fontSize={48} fontWeight={700} textAlign="center">
-        Private Page
-      </Typography> */}
+    <Box mt={10}>
       <Stack justifyContent="center" alignItems="center">
         <iframe
           src="https://trading.oxsecurities.com/open-live-account?iframe=1&unbranded=1"
           title="Iframe Example"
-          className="w-full h-lg"
+          className="w-full h-screen"
         ></iframe>
       </Stack>
     </Box>
