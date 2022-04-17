e09f90faf032210b1f931280370a1ef2aa7ee9de
DejanTeofilovic <dejanteofilovic2@gmail.com>
2022-04-18 01:40:33 +0900

    server - update

--- server.js
+++ server.js
@@ -26,20 +26,20 @@ app.use('/api/whitelist', require('./routes/whitelistRoute'));
 app.use('/api/admin', require('./routes/adminRoute'));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//   // Set static folder
+//   app.use(express.static('client/build'));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
-// app.use(express.static('client/build'));
+app.use(express.static('client/build'));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
