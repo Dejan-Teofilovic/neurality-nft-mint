fc777cd034a17a203acbda511b50dc871bbbe372
DejanTeofilovic <dejanteofilovic2@gmail.com>
2022-06-16 15:52:55 +0900

    description fix

--- client/src/pages/HomePage/WhoWeAreSection.js
+++ client/src/pages/HomePage/WhoWeAreSection.js
@@ -19,35 +19,35 @@ import MHidden from '../../components/@mui-extend/MHidden';
 
 const DATA_OF_MEMBERS = [
   {
-    id: 1,
-    name: 'Sid',
-    position: 'Algotrader',
-    description: 'Behind the pseudonym "Sid" is the algotrader of the Neurality algorithm. He’s got a CFA (Chartered Financial Analyst) and a FRM (Financial Risk Management) graduation. He’s passionate about algotrading and quant strategies and he’s got a 11 years experience as a trader and more importantly a 5 years experience as an algotrader with a renowned algotrading company. He is also very interested in DeFi and cryptocurrencies. Thanks to his expertise and work we can offer you such a service today.',
-    image: '/assets/images/sid.png',
-    linkedinUrl: 'https://www.linkedin.com/in/fredericpaulus/'
-  },
-  {
     id: 2,
     name: 'Pirate',
     position: 'Founder (CEO)',
-    description: 'Behind the pseudonym "Pirate" is Founder (CEO). He has several years of experience in algotrading tests, he is also passionate about DeFi and crypto-currencies. He is the one who writes the lines you are reading and is responsible for all the artistic direction of the project (NFT, visuals, music etc), as well as the marketing, copywriting, social networks part. He is the brain behind many of the ideas and initiatives on the Neurality project.',
+    description: 'Pirate is the founder of the project. He has several years of experience in algotrading tests, his ambition is to bring this market to new horizons thanks to DeFi and the democratisation of NFTs. He is the one who writes the lines you are reading and is responsible for the whole artistic direction of the project (NFT, visuals, music etc), as well as the marketing, copywriting and social media part. He is the brain behind many of the ideas and initiatives of the Neurality project.',
     image: '/assets/images/pirate.png',
     linkedinUrl: 'https://www.linkedin.com/in/jerome-santangelo-256b7011a/'
+  },
+  {
+    id: 1,
+    name: 'Sid',
+    position: 'Algotrader',
+    description: 'Sid is the algotrader of the Neurality algorithm. He is a CFA (Chartered Financial Analyst) and FRM (Financial Risk Management) charterholder. He is passionate about algotrading and quant strategies and has 11 years of experience as a trader and especially 5 years of experience as an algotrader in a renowned algotrading company. He is also very interested in DeFi and cryptocurrencies. It is thanks to his expertise and work that we can now offer you such a service.',
+    image: '/assets/images/sid.png',
+    linkedinUrl: 'https://www.linkedin.com/in/fredericpaulus/'
   },
   {
     id: 3,
     name: 'New',
     position: 'Developer',
-    description: 'Behind the name "New" is the Web3 developer of our team. He is the one who coded the site you are currently on. He is an experienced Web and Blockchain developer. He started web development when he was a student and after graduation, he jumped into the ocean of Blockchain. Until now, he has developed some sites for E-Commerce, HR management, NFT marketplace and NFT mint. Currently he is working on some Blockchain projects including Neurality.',
+    description: 'New is the Web3 developer of the team. He is the one who coded the site you are currently on. He is an experienced web and blockchain developer. He started web development as a student and after graduation he jumped into the blockchain ocean. So far he has developed sites for e-commerce, human resource management, NFT marketplace and NFT currency. He is currently working on blockchain projects, including Neurality.',
     image: '/assets/images/new.jpg',
     linkedinUrl: 'https://www.linkedin.com/in/maverick-sin-86966a229/'
   },
   {
     id: 4,
     name: 'Ben',
-    position: 'Dummy',
+    position: 'Broker Partner',
     description: 'Ben has been working the FX brokering industry for almost a decade and has held numerous leadership roles in some of the largest brokers globally including OANDA and Axi. For the last 2 years Ben has been running Ox Securities which has grown to be a real global player in the FX and Crypto space. Ben brings expertise in management account technology and customised liquidity solutions.',
-    image: '/assets/images/ben.png',
+    image: '/assets/images/ben.jpg',
     linkedinUrl: 'https://www.linkedin.com/in/ben-malone-737144143/'
   }
 ];
